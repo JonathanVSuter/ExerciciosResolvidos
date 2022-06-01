d83fedf3d2817535b2794cc62a99048aa137c0c7
JONATHAN VINICIUS SUTER <jonathan.suter@prof.sc.senac.br>
2022-05-31 22:33:46 -0300

    Revisão

---
+++ RevisaoGeralCSharp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace RevisaoGeralCSharp
+{
+    class Program
+    {
+        //Exercício 1
+        /*1- Faça um programa que receba dois números que o usuário digitou, 
+        faça a soma e mostre na tela*/
+        //static void Main(string[] args)
+        //{
+        //    double n1;
+        //    double n2;
+
+        //    Console.WriteLine("Insira o valor para n1");
+        //    n1 = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Insira o valor para n2");
+        //    n2 = Convert.ToDouble(Console.ReadLine());
+        //    double res = n1 + n2;
+        //    Console.WriteLine("O Resultado da soma é" + res);
+        //}
+        /*Exercício 2: Escreva um programa que receba 2 valores e mostre 
+         * o maior entre eles;
+         */
+
+        //static void Main(string[] args)
+        //{
+        //    double n1;
+        //    double n2;
+
+        //    Console.WriteLine("Insira o valor para n1");
+        //    n1 = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Insira o valor para n2");
+        //    n2 = Convert.ToDouble(Console.ReadLine());
+
+        //    if (n1>n2) 
+        //    {
+        //        Console.WriteLine("O maior valor é n1");
+        //    }
+        //    else if (n2>n1) 
+        //    {
+        //        Console.WriteLine("O maior valor é n2");
+        //    }
+        //    else 
+        //    {
+        //        Console.WriteLine("Os valores são iguais");
+        //    }           
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int anoDeNascimento;            
+
+        //    Console.WriteLine("Insira o seu ano de nascimento:");
+        //    anoDeNascimento = Convert.ToInt32(Console.ReadLine());
+
+        //    //isso:
+        //    //int res = DateTime.Now.Year - anoDeNascimento;
+        //    //é o mesmo que isso:
+        //    int res = 2022 - anoDeNascimento;
+
+        //    Console.WriteLine("Sua idade é"+ res);            
+        //}
+
+        static void Main(string[] args)
+        {
+            double x;
+
+            x = 8 / 2;
+
+            Console.WriteLine("O resultado é "+x);
+        }
+
+    }
+}
+
